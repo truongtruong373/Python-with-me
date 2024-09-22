5742432c64bf8822f500fd378cc120db310db48b
truongtruong373 <truongtd373@gmail.com>
2024-09-22 17:28:18 +0700

    truong's message

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
-# import torch
+import torch
 
-# # Kiểm tra xem CUDA có khả dụng không
-# if torch.cuda.is_available():
-#     # Lấy số lượng GPU
-#     num_devices = torch.cuda.device_count()
-#     print(f"Số lượng GPU khả dụng: {num_devices}")
+# Kiểm tra xem CUDA có khả dụng không
+if torch.cuda.is_available():
+    # Lấy số lượng GPU
+    num_devices = torch.cuda.device_count()
+    print(f"Số lượng GPU khả dụng: {num_devices}")
     
-#     # Lấy tên của từng GPU
-#     for i in range(num_devices):
-#         print(f"GPU {i}: {torch.cuda.get_device_name(i)}")
-# else:
-#     print("CUDA không khả dụng.")
+    # Lấy tên của từng GPU
+    for i in range(num_devices):
+        print(f"GPU {i}: {torch.cuda.get_device_name(i)}")
+else:
+    print("CUDA không khả dụng.")
 print("my name is truong")
